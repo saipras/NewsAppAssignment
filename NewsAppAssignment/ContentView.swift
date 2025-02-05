9d134d86cccb55995a7122cd30377383962283e5
sai-prasad-rekha <saiprasadrekha881@gmail.com>
2025-02-05 14:23:20 +0530

    Initial Commit

---
+++ NewsAppAssignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewsAppAssignment
+//
+//  Created by Sai Prasad Rekha on 05/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewsAppAssignment/NewsAppAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppAssignmentApp.swift
+//  NewsAppAssignment
+//
+//  Created by Sai Prasad Rekha on 05/02/25.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
